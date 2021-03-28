20b5aa594b3f9e8b75980a906fee4aee1d0ff760
khayri-bo <80332795+khayri-bo@users.noreply.github.com>
2021-03-28 05:30:40 -0700

    Update background.c

--- background.c
+++ background.c
@@ -1,14 +1,14 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include "background.h"
-void initBckg (background * b, char url[]){
+void initBackground (background * b, char url[]){
 b->img = IMG_Load (url);
 b->bck.x = 0;
 b->bck.y = 0;
 b->bck.w = b->img->w;
 b->bck.h = b->img->h;
 }
-void showBckg (SDL_Surface * screen, background b){
+void showBacckground (SDL_Surface * screen, background b){
 SDL_BlitSurface (b.img, &(b.bck), screen, NULL);
 }
 void scrollToLeft (background * b){
