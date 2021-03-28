df80dcd5b85bd05d467e47a37092dc008c46fc07
khayri-bo <80332795+khayri-bo@users.noreply.github.com>
2021-03-28 05:30:44 -0700

    Update background.h

--- background.h
+++ background.h
@@ -3,12 +3,12 @@
 #define CAMERA_H 200
 struct background
 {
-SDL_Rect bckg;
+SDL_Rect bck;
 SDL_Surface *img;
 };
 typedef struct background background;
-void initBckg (background * b, char url[]);
-void showBckg (SDL_Surface * screen, background b);
+void initBackground (background * b, char url[]);
+void showBacckground (SDL_Surface * screen, background b);
 void scrollToLeft (background * b);
 void scrollToRight (background * b);
 
