f83e76a4603db6fe79342dda4f1540abfe7e95f8
khayri-bo <80332795+khayri-bo@users.noreply.github.com>
2021-03-30 09:35:18 +0100

    Update background.h

--- background.h
+++ background.h
@@ -8,7 +8,7 @@ SDL_Surface *img;
 };
 typedef struct background background;
 void initBackground (background * b, char url[]);
-void showBacckground (SDL_Surface * screen, background b);
+void showBackground (SDL_Surface * screen, background b);
 void scrollToLeft (background * b);
 void scrollToRight (background * b);
 
